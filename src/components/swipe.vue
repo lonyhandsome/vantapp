71aec1d00c0c8e30b260ca0af12d143e43fc0908
zhuhuajun <55329462@qq.com>
2020-04-17 17:11:49 +0800

    ~

---
+++ src/components/swipe.vue
@@ -0,0 +1,33 @@
+<template>
+  <van-sticky>
+    <van-swipe class="my-swipe" :autoplay="3000" indicator-color="white">
+      <van-swipe-item>广告位出租</van-swipe-item>
+      <van-swipe-item>广告位出租</van-swipe-item>
+      <van-swipe-item>广告位出租</van-swipe-item>
+      <van-swipe-item>广告位出租</van-swipe-item>
+    </van-swipe>
+  </van-sticky>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  data() {
+    return {
+      active: 0
+    };
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.my-swipe .van-swipe-item {
+  margin-top: 1vh;
+  color: #fff;
+  font-size: 20px;
+  line-height: 150px;
+  text-align: center;
+  background-color: gray;
+}
+</style>
---
+++ src/components/top.vue
@@ -0,0 +1,53 @@
+<style scoped>
+.van-popup {
+  width: 70vw;
+  height: 110vh;
+}
+.van-icon {
+  font: left;
+  margin-top: 2.5vh;
+  margin-left: 3.5vw;
+}
+.title {
+  height: 10vh;
+  line-height: 8vh;
+  background-color: white;
+  margin-bottom: 5vh;
+}
+</style>
+<template>
+  <div>
+    <van-sticky>
+      <div class="title">
+        <van-row>
+          <van-col span="2">
+            <van-icon name="manager" size="1.3rem" @click="showPopup" />
+          </van-col>
+          <van-col span="20">平头速运</van-col>
+        </van-row>
+        <van-notice-bar text="超多电子首存返水1.8起公司入款2%笔笔送" left-icon="volume-o" mode="closeable" />
+      </div>
+    </van-sticky>
+    <van-popup v-model="show" position="left">今日</van-popup>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      active: 0,
+      show: false
+    };
+  },
+  methods: {
+    showPopup() {
+      console.log("click");
+      this.show = true;
+    }
+  }
+};
+</script>
+
+<style scoped>
+</style>
